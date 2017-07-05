844347f05aaa789410c9cbbc109347670eb3f083
littleGXX <1344704054@qq.com>
2017-07-05 11:57:51 +0800

    hello

---
+++ nowcoder/src/com/nowcoder/Main.java
@@ -0,0 +1,10 @@
+package com.nowcoder;
+
+/**
+ * Created by GXiang on 2017/7/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello nowcoder");
+    }
+}
